3008d3f1f45e11781341a82884d7888e5008d411
fizzywormbot <wormbot2406@gmail.com>
2024-04-07 16:01:13 -0700

    Fixed translation sequence bug

--- stepper_motor_controls.py
+++ stepper_motor_controls.py
@@ -7,9 +7,9 @@ from system_config import *
 motor_step_size = 1.8
 microsteps = 8
 steps_per_rev = 360/(motor_step_size)*microsteps
-pitch_circumference = math.pi * 12 # mm
+pitch_circumference = math.pi * 40
 gear_ratio = 40/12
-distance_per_step = pitch_circumference / steps_per_rev * gear_ratio # in mm per step
+# distance_per_step = pitch_circumference / steps_per_rev * gear_ratio # in mm per step
 wait_time_s = 0.0025 # frequency of 200Hz with period of 5ms
 
 wait_dir_s = 0.001 # 1 ms
@@ -18,43 +18,13 @@ wait_ena_s = 0.2 # 200ms
 # GPIO pin assignment
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
 
-pul_1 = 2
-dir_1 = 3
-ena_1 = 4
-
-pul_3 = 17
-dir_3 = 27
-ena_3 = 22
-
-pul_4 = 10
-dir_4 = 9
-ena_4 = 11
-
-pul_6 = 5
-dir_6 = 6
-ena_6 = 13
-
-pul_2 = 14
-dir_2 = 15
-ena_2 = 18
-
-pul_5 = 25
-dir_5 = 8
-ena_5 = 7
-
-# feel free to rename
-# furthest aka the one farthest from u on the rack
-limit_switch_furthest = 21
-limit_switch_middle = 20
-limit_switch_closest = 16
-
 motors = {
-    TRA_1: {"pulse": pul_1, "dir": dir_1, "enable": ena_1},
-    ROT_1: {"pulse": pul_2, "dir": dir_2, "enable": ena_2},
-    TRA_2: {"pulse": pul_4, "dir": dir_4, "enable": ena_4},
-    ROT_2: {"pulse": pul_3, "dir": dir_3, "enable": ena_3},
-    TRA_3: {"pulse": pul_5, "dir": dir_5, "enable": ena_5},
-    ROT_3: {"pulse": pul_6, "dir": dir_6, "enable": ena_6},
+    TRA_1: {"pulse": PUL_1, "dir": DIR_1, "enable": ENA_1},
+    ROT_1: {"pulse": PUL_2, "dir": DIR_2, "enable": ENA_2},
+    TRA_2: {"pulse": PUL_4, "dir": DIR_4, "enable": ENA_4},
+    ROT_2: {"pulse": PUL_3, "dir": DIR_3, "enable": ENA_3},
+    TRA_3: {"pulse": PUL_5, "dir": DIR_5, "enable": ENA_5},
+    ROT_3: {"pulse": PUL_6, "dir": DIR_6, "enable": ENA_6},
 }
 
 # GPIO output
@@ -63,8 +33,10 @@ for _, pins in motors.items():
     GPIO.setup(pins["dir"], GPIO.OUT)
     GPIO.setup(pins["enable"], GPIO.OUT)
 
-# GPIO pins still available to use
-# GPIO: 19, 26, 23, 24, 12, 16, 20 21
+# set-up limit switches
+GPIO.setup(LIMIT_SWITCH_1, GPIO.IN)
+GPIO.setup(LIMIT_SWITCH_2, GPIO.IN)
+GPIO.setup(LIMIT_SWITCH_3, GPIO.IN)
 
 # function that takes array of degrees for each motor to turn
 # and then turns that amt -> translates to number of steps
@@ -93,20 +65,20 @@ def turn_degree(degrees):
             time.sleep(wait_time_s)
 
 def translate_steps(distance):
-    for motor_num, motor_translation in distance.iter():
+    for motor_num, motor_translation in distance.items():
         # Step 1: Set ENA to HIGH; ENA must be ahead of DIR by at least 200ms
         GPIO.output(motors[motor_num]["enable"], True)
         time.sleep(wait_ena_s)
     
         # Step 2: Set DIR; DIR must be ahead of PUL effective edge by 2us to ensure correct direction
-        if distance >= 0:
+        if motor_translation >= 0:
             GPIO.output(motors[motor_num]["dir"], True)
         else:
             GPIO.output(motors[motor_num]["dir"], False)
         time.sleep(wait_dir_s)
 
         # Step 3: Pulse the proper amount
-        num_steps = math.ceil(distance / distance_per_step)
+        num_steps = math.ceil(math.fabs(motor_translation) * steps_per_rev * gear_ratio / pitch_circumference)
         pulse_pin = motors[motor_num]["pulse"]
         for _ in range(num_steps):
             GPIO.output(pulse_pin,True)
@@ -129,12 +101,15 @@ try:
     # turn_degree([0,0,0,0,90,0])
 
     # turn_degree([0,90,0,90,0,90])
-    motion_list = [
-        [0,120,0,180,0,180],
-    ]
+    # motion_list = [
+    #     [0,120,0,180,0,180],
+    # ]
 
-    for motion in motion_list:
-        turn_degree(motion)
+    # for motion in motion_list:
+    #     turn_degree(motion)
+
+    motion_list = {TRA_1:-30, ROT_1: 0, TRA_2: 0, ROT_2: 0,TRA_3: 0,ROT_3: 0}
+    translate_steps(motion_list)
 
     time.sleep(3)
     # shut down pins
@@ -145,6 +120,6 @@ try:
         
 except KeyboardInterrupt:
     print("Ctrl+C detected. Cleaning up GPIO.")
-    for pul in [pul_1, pul_2, pul_3, pul_4, pul_5, pul_6]:
+    for pul in [PUL_1, PUL_2, PUL_3, PUL_4, PUL_5, PUL_6]:
         GPIO.output(pul,False)
     # GPIO.cleanup()
--- system_config.py
+++ system_config.py
@@ -4,3 +4,35 @@ TRA_2 = 2
 ROT_2 = 3
 TRA_3 = 4
 ROT_3 = 5
+
+PUL_2 = 2
+DIR_2 = 3
+ENA_2 = 4
+
+PUL_4 = 17
+DIR_4 = 27
+ENA_4 = 22
+
+PUL_3 = 10
+DIR_3 = 9
+ENA_3 = 11
+
+PUL_5 = 5
+DIR_5 = 6
+ENA_5 = 13
+
+PUL_1 = 14
+DIR_1 = 15
+ENA_1 = 18
+
+PUL_6 = 25
+DIR_6 = 8
+ENA_6 = 7
+
+# small number corresponds to the stage closer to the RPi
+LIMIT_SWITCH_3 = 21
+LIMIT_SWITCH_2 = 20
+LIMIT_SWITCH_1 = 16
+
+# GPIO pins still available to use
+# GPIO: 19, 26, 23, 24, 12, 16, 20 21
